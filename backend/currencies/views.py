bc086d06d86ceedb4b996b0f7d87c033438d0c8b
Martyna Witkowska <mar.witkowska@gmail.com>
2023-06-07 10:33:20 +0200

    basic logic for retrieving currency data from local database

--- backend/currencies/views.py
+++ backend/currencies/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
 from django.views.generic import FormView
 
-from . import forms
+from . import forms, models
 
 
 class CurrencyFormView(FormView):
     form_class = forms.CurrencyForm
     template_name = "currencies/currency_form.html"
     success_url = "/"
+
+    def form_valid(self, form):
+        start_date = form.cleaned_data.get("start_date")
+        end_date = form.cleaned_data.get("end_date")
+        currencies = models.CurrencyName.objects.all()
+
+        currency_data = models.CurrencyValue.objects.filter(currency_date__date__range=(start_date, end_date))
+
+        context = self.get_context_data()
+        context.update({"currency_data": currency_data})
+
+        return self.render_to_response(context)
+
+    def get_form(self, form_class=None):
+        currencies = models.CurrencyName.objects.all()
+        form = super().get_form(form_class)
+
+        form.fields["currency"].choices = [(currency.code, currency.name.title()) for currency in currencies]
+
+        return form
