957462eef528f86be7cf642b5768baa8fb4e5e82
Martyna Witkowska <mar.witkowska@gmail.com>
2023-06-07 13:15:58 +0200

    validation for date fields and test for validators

--- backend/currencies/forms.py
+++ backend/currencies/forms.py
@@ -1,7 +1,30 @@
 from django import forms
+from django.utils import timezone
 
 
 class CurrencyForm(forms.Form):
     start_date = forms.DateField(widget=forms.TextInput(attrs={"type": "date"}))
     end_date = forms.DateField(widget=forms.TextInput(attrs={"type": "date"}))
     currency = forms.MultipleChoiceField(choices=[], widget=forms.SelectMultiple)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
+
+        if start_date and start_date < timezone.datetime(2005, 1, 2).date():
+            self.add_error("start_date", "Start date cannot be earlier than 2005-01-02.")
+
+        if start_date and start_date > timezone.now().date():
+            self.add_error("start_date", "Start date cannot be in the future.")
+
+        if end_date and end_date > timezone.now().date():
+            self.add_error("end_date", "End date cannot be in the future.")
+
+        if end_date >= timezone.now().date():
+            self.add_error("end_date", "End date must be earlier than today.")
+
+        if start_date and end_date and end_date < start_date:
+            self.add_error("end_date", "End date cannot be earlier than start date.")
+
+        return cleaned_data
---
+++ backend/currencies/tests/test_forms.py
@@ -0,0 +1,90 @@
+import pytest
+from django.utils import timezone
+
+from ..forms import CurrencyForm
+
+
+@pytest.fixture
+def currency_choices():
+    return [("USD", "dolar amerykański"), ("EUR", "euro")]
+
+
+def test_currency_form_valid_data(currency_choices):
+    form = CurrencyForm(
+        data={
+            "start_date": "2022-01-01",
+            "end_date": "2022-01-10",
+            "currency": ["USD", "EUR"],
+        }
+    )
+
+    print("*" * 20)
+    print("*" * 20)
+    print("*" * 20)
+    print(timezone.now().date())
+    print("*" * 20)
+    print("*" * 20)
+    print("*" * 20)
+
+    form.fields["currency"].choices = currency_choices
+
+    assert form.is_valid()
+
+
+def test_currency_form_invalid_start_date(currency_choices):
+    form = CurrencyForm(
+        data={
+            "start_date": "2000-01-01",
+            "end_date": "2022-01-10",
+            "currency": ["USD", "EUR"],
+        }
+    )
+
+    form.fields["currency"].choices = currency_choices
+
+    assert not form.is_valid()
+    assert "Start date cannot be earlier than 2005-01-02." in str(form.errors)
+
+
+def test_currency_form_invalid_start_date_future(currency_choices):
+    future_date = timezone.now().date() + timezone.timedelta(days=1)
+
+    form = CurrencyForm(data={"start_date": str(future_date), "end_date": "2022-01-10", "currency": ["USD", "EUR"]})
+
+    form.fields["currency"].choices = currency_choices
+
+    assert not form.is_valid()
+    assert "Start date cannot be in the future." in str(form.errors)
+
+
+def test_currency_form_invalid_end_date_before_start_date(currency_choices):
+    form = CurrencyForm(data={"start_date": "2022-01-10", "end_date": "2022-01-01", "currency": ["USD", "EUR"]})
+
+    form.fields["currency"].choices = currency_choices
+
+    assert not form.is_valid()
+    assert "End date cannot be earlier than start date." in str(form.errors)
+
+
+def test_end_date_not_later_than_today(currency_choices):
+    form = CurrencyForm(
+        data={"start_date": "2022-01-01", "end_date": timezone.now().date(), "currency": ["USD", "EUR"]}
+    )
+
+    form.fields["currency"].choices = currency_choices
+
+    assert not form.is_valid()
+    assert "End date must be earlier than today." in str(form.errors)
+
+
+def test_end_date_not_in_future(currency_choices):
+    form = CurrencyForm(
+        data={
+            "start_date": "2022-01-01",
+            "end_date": str(timezone.now().date() + timezone.timedelta(days=1)),
+            "currency": ["USD", "EUR"],
+        }
+    )
+
+    assert not form.is_valid()
+    assert "End date cannot be in the future." in str(form.errors)
